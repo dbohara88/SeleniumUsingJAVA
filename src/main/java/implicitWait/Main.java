a86f9e88cd61fea4198cccd2af9f3ce90a5e761b
Deepak Bohara <65617245+dbohara88@users.noreply.github.com>
2025-11-05 20:35:12 +0530

    implicit wait

--- src/main/java/implicitWait/Main.java
+++ src/main/java/implicitWait/Main.java
@@ -1,16 +1,29 @@
 package implicitWait;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		WebDriver driver = new ChromeDriver();
 		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--incognito");
+		
+		
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath("//*[@title='Search']")).sendKeys("Selenium");
+		driver.findElement(By.xpath("//*[@value='Google Search']")).click();
+
 	}
 
 }
